5d64a7a76c1fb004f66d3726af0f2a0a838095d9
Will Jones <willjones2010@gmail.com>
2020-04-02 00:05:53 -0400

    stop button

--- metronome.js
+++ metronome.js
@@ -10,12 +10,22 @@ let defaultMetronomeOptions = {
 
 
 const startButton = document.getElementById("startButton")
-
+const stopButton = document.getElementById("stopButton")
 
 startButton.addEventListener("click", function (event) {
     let time = parseInt(document.getElementById("time").value)
     event.preventDefault()
     setInterval(function () { console.log(`sound every ${time}`) }, time)
+    // startButton.disabled = true
+    // stopButton.disabled = false
 
+    stopButton.addEventListener("click", function (event) {
+        clearInterval()
+        // startButton.disabled = false
+        // stopButton.disabled = true
+
+    })
 })
+
+
 
